bc199587f8f78d2dbc1db716d3237abcbbe2e771
Chan Pham <iamchanpham@gmail.com>
2024-09-14 13:45:00 +0000

    Practice template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CRUD-practice
+These exercises will help you practice the competencies in tomorrow's lab. 
+Start with the section you feel least confident about and complete them all to feel prepared!
---
+++ index.js
@@ -0,0 +1,71 @@
+// These exercises will help you practice for the lab next class
+// PRIMITIVE DATA TYPES & VARIABLES (1.4)
+// Prompt the user to enter their first name and save it in a variable
+
+// Prompt the user to enter their last name and save it in a variable
+
+// Use string concatenation or template literals to console log "Welcome [first name] [last name]""
+
+// Prompt the user to enter their age 
+
+// Cast it to the correct type
+
+// Use string concatenation or template literals to console log "Next year you will be [age + 1] years old"
+
+
+// CONDITIONAL STATEMENTS (1.6)
+// Prompt the user to answer if it's hot today
+
+// Write an if/else statement such that
+// if the user says "yes", console log "Stay hydrated"
+// else console log "Stay chill"
+
+
+let randomNumber = Math.random()
+// Prompt the user to guess a number between 0-1
+
+// Cast it to the correct type
+
+// Write an if/else statement such that
+// if the number is lower than the random number, say "Wrong, too low"
+
+// else if the number is higher than the random number, say "Wrong, too high"
+
+// else, say "Correct!"
+
+// After the if/else statement, use string concatenation/template literals to print "The number was [random number]"
+
+// ARRAYS (1.5)
+// Create an array called friends with at least 2 friends (they don't have to be real!)
+
+// Console log the first element
+
+// Remove the first element from the array.
+
+// Add another friend to the end of the array
+
+// Use string concatenation/template literals to print "I have [length of array] friends"
+
+// Prompt the user to enter a number for which friend they want to see
+
+// Cast it to the correct type and subtract 1 from it
+
+// Console log the friend at the index specified (format it into a nice sentence if you can!)
+
+// OBJECTS (1.5)
+// Create a character object with 3 key:value pairs (e.g. name, show, age)
+
+// Console log the name of the character
+
+// Modify one of the keys to be a different value
+
+// Add a new key value pair to the object that is an array (e.g. friends, catchphrases)
+
+// STRETCH
+// Make an array called characters that has at least 3 character objects in it
+// You can make the objects directly in the array or push the objects to the array
+
+
+// Console log the first character
+
+// Console log the name of the first character
